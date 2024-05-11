cc19dafa6cd60383999bc019fe996fabbb52c2c4
ZarusILie <lazarusliekarsono@gmail.com>
2024-05-11 22:28:48 +0700

    update baseURL

--- api/http-common.js
+++ api/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://virtualtapcash-production.up.railway.app/api/v1",
+  baseURL: "http://34.128.83.116:8080/api/v1",
   headers: {
     "Content-type": "application/json",
   },
--- api/http-get.js
+++ api/http-get.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const apiGet = axios.create({
-  baseURL: "https://virtualtapcash-production.up.railway.app/api/v1",
+  baseURL: "http://34.128.83.116:8080/api/v1",
   headers: {
     // 'ngrok-skip-browser-warning': true,
   },
